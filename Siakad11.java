54749b11ab317c40d1e691f388f3b42d8278fa1c
marsya505 <marsyazulfazahrani0@gmail.com>
2024-09-13 12:15:31 +0700

    Add files via upload

--- Siakad11.java
+++ Siakad11.java
@@ -6,27 +6,29 @@ public class Siakad11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String nama, nim;
-        char kelas;
+        String kelas;
         byte absen;
-        double nilaiKuis, nilaiTugas,nilaiUjian, nilaiAkhir;
+        double quizscore, assignmentscore, UTSscore, UASscore, finalscore;
         System.out.print("Masukkan nama: ");
         nama = sc.nextLine();
         System.out.print("Masukkan NIM: ");
         nim = sc.nextLine();
         System.out.print("Masukkan kelas: ");
-        kelas = sc.nextLine().charAt(0);
+        kelas = sc.nextLine();
         System.out.print("Masukkan nomor absen: ");
         absen = sc.nextByte();
-        System.out.print("Masukkan nilai kuis; ");
-        nilaiKuis = sc.nextDouble();
+        System.out.print("Masukkan nilai kuis: ");
+        quizscore = sc.nextDouble();
         System.out.print("Masukkan nilai tugas: ");
-        nilaiTugas = sc.nextDouble();
-        System.out.print("Masukkan nilai ujian: ");
-        nilaiUjian = sc.nextDouble();
-        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian ) / 3;
+        assignmentscore = sc.nextDouble();
+        System.out.print("Masukkan nilai UTS: ");
+        UTSscore = sc.nextDouble();
+        System.out.print("Masukkan nilai UAS: ");
+        UASscore = sc.nextDouble();
+        finalscore = (quizscore * 0.20) + (assignmentscore * 0.15) + (UTSscore * 0.30) + (UASscore * 0.35);
         System.out.println("Nama: " + nama + "NIM: " + nim);
         System.out.println("Kelas: " + kelas + "Absen: " + absen);
-        System.out.println("Nilai Akhir: " + nilaiAkhir);
+        System.out.println("Nilai Akhir: " + finalscore);
 
     }
 }
